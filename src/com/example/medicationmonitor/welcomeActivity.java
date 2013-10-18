805c83641a5e88ab250ee25de3f7344ac43dce70
deemsys <sangeetha_d@deemsysinc.com>
2013-10-18 09:32:12 -0700

    tab bar implementation removed

--- src/com/example/medicationmonitor/welcomeActivity.java
+++ src/com/example/medicationmonitor/welcomeActivity.java
@@ -1,11 +1,25 @@
 package com.example.medicationmonitor;
 
 
-import android.app.Activity;
-import android.content.Intent;
+
 
 import android.os.Bundle;
+import android.app.Activity;
+//import android.app.AlertDialog;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+//import android.content.DialogInterface;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+//import android.widget.ExpandableListView.OnGroupClickListener;
+//import android.widget.ExpandableListView.OnGroupCollapseListener;
+//import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
 
 import android.view.View;
 
@@ -14,9 +28,13 @@ import android.widget.TextView;
 
 
 public class welcomeActivity extends Activity  {
+	Button medications;
 	TextView textview;
-	TextView signout; 
-	// private FragmentTabHost mTabHost;
+	TextView signout;
+	ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
 	
 	@Override
@@ -24,40 +42,19 @@ public class welcomeActivity extends Activity  {
         super.onCreate(savedInstanceState);
         // setting default screen to login.xml
         setContentView(R.layout.welcome);
+        expListView = (ExpandableListView) findViewById(R.id.expandableListView1);
         
-     /*   mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
-        mTabHost.setup(this, getSupportFragmentManager(), R.id.tabFrameLayout);
-        mTabHost.addTab(
-                mTabHost.newTabSpec("tab1").setIndicator("Tab 1",
-                        getResources().getDrawable(android.R.drawable.star_on)),
-                FragmentTab.class, null);
-        mTabHost.addTab(
-                mTabHost.newTabSpec("tab2").setIndicator("Tab 2",
-                        getResources().getDrawable(android.R.drawable.star_on)),
-                FragmentTab.class, null);
-        mTabHost.addTab(
-                mTabHost.newTabSpec("tab3").setIndicator("Tab 3",
-                        getResources().getDrawable(android.R.drawable.star_on)),
-                FragmentTab.class, null);
-        mTabHost.addTab(
-                mTabHost.newTabSpec("tab4").setIndicator("Tab 4",
-                        getResources().getDrawable(android.R.drawable.star_on)),
-                FragmentTab.class, null);
-        mTabHost.addTab(
-                mTabHost.newTabSpec("tab5").setIndicator("Tab 5",
-                        getResources().getDrawable(android.R.drawable.star_on)),
-                FragmentTab.class, null);
+        prepareListData();
+        
 
 
-        mTabHost.addTab(
-                mTabHost.newTabSpec("tab7").setIndicator("Tab 7",
-                        getResources().getDrawable(android.R.drawable.star_on)),
-                FragmentTab.class, null);
-       
+        
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+ 
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+ 
       
-        
-      */
-        
         Intent in = getIntent();
         if (in.getCharSequenceExtra("usr") != null) {
         	final TextView setmsg = (TextView)findViewById(R.id.welcome);
@@ -76,7 +73,61 @@ public class welcomeActivity extends Activity  {
                 finish();
             }
         });
+        Button medication = (Button) findViewById(R.id.medications);
+      	 
+        // Listening to Login Screen link
+        medication.setOnClickListener(new View.OnClickListener() {
+ 
+            public void onClick(View arg0) {
+            	/// Create Intent for SignUpActivity  and Start The Activity
+    			Intent medication=new Intent(getApplicationContext(),Medications.class);
+    			startActivity(medication);                 // Closing registration screen
+                // Switching to Login Screen/closing register screen
+                finish();
+            }
+        });
+        
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+        	 
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                    int groupPosition, int childPosition, long id) {
+            	
+            	//Intent sigout=new Intent(getApplicationContext(),LoginActivity.class);
+    			//startActivity(sigout);
+               // TODO Auto-generated method stub
+                Toast.makeText(
+                      getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                             + " : "
+                       + listDataChild.get(
+                                       listDataHeader.get(groupPosition)).get(
+                                     childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+        
 	}
+	 private void prepareListData() {
+	        listDataHeader = new ArrayList<String>();
+	        listDataChild = new HashMap<String, List<String>>();
+	 
+	        // Adding child data
+	        listDataHeader.add("Medications");
+	        listDataHeader.add("Reminders");
+	        listDataHeader.add("Appointments");
+	        List<String> Medications = new ArrayList<String>();
+	        List<String> Reminders= new ArrayList<String>();
+	        List<String> Appointments = new ArrayList<String>();
+	      
+	        
+	        
+	        listDataChild.put(listDataHeader.get(0), Medications); 
+	        listDataChild.put(listDataHeader.get(1), Reminders);
+	        listDataChild.put(listDataHeader.get(2), Appointments);       
+	 
+	 }
 }
 
 	        	
