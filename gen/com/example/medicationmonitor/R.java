e7ff86b23659833dc81bec087a7a8f01540efe50
deemsys <sangeetha_d@deemsysinc.com>
2013-10-18 09:42:34 -0700

    tab bar modifications

--- gen/com/example/medicationmonitor/R.java
+++ gen/com/example/medicationmonitor/R.java
@@ -8,71 +8,143 @@
 package com.example.medicationmonitor;
 
 public final class R {
-    public static final class array {
-        public static final int age_array=0x7f060000;
-    }
     public static final class attr {
     }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
+        /** 
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f040006;
+        public static final int activity_vertical_margin=0x7f040000;
+        public static final int list_item_height=0x7f040005;
+        public static final int tab_height=0x7f040001;
+        public static final int tab_padding=0x7f040002;
+        public static final int text_default=0x7f040004;
+        public static final int text_small=0x7f040003;
     }
     public static final class drawable {
-        public static final int bg=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int login=0x7f020002;
-        public static final int logo=0x7f020003;
-        public static final int logo_img=0x7f020004;
-        public static final int registration=0x7f020005;
-        public static final int signin=0x7f020006;
-        public static final int signup=0x7f020007;
-        public static final int splash=0x7f020008;
+        public static final int addmeds=0x7f020000;
+        public static final int appointments1=0x7f020001;
+        public static final int appointments2=0x7f020002;
+        public static final int back=0x7f020003;
+        public static final int bg=0x7f020004;
+        public static final int communicate1=0x7f020005;
+        public static final int communicate2=0x7f020006;
+        public static final int edittext_img=0x7f020007;
+        public static final int edittextrest=0x7f020008;
+        public static final int gradient_bg=0x7f020009;
+        public static final int gradient_bg_hover=0x7f02000a;
+        public static final int home1=0x7f02000b;
+        public static final int home2=0x7f02000c;
+        public static final int ic_launcher=0x7f02000d;
+        public static final int image_bg=0x7f02000e;
+        public static final int list_selector=0x7f02000f;
+        public static final int login=0x7f020010;
+        public static final int loginimg=0x7f020011;
+        public static final int logo=0x7f020012;
+        public static final int logo_img=0x7f020013;
+        public static final int medications1=0x7f020014;
+        public static final int medications2=0x7f020015;
+        public static final int questionnaire1=0x7f020016;
+        public static final int questionnaire2=0x7f020017;
+        public static final int registration=0x7f020018;
+        public static final int registrationnew=0x7f020019;
+        public static final int reminders1=0x7f02001a;
+        public static final int reminders2=0x7f02001b;
+        public static final int save=0x7f02001c;
+        public static final int settings1=0x7f02001d;
+        public static final int settings2=0x7f02001e;
+        public static final int signin=0x7f02001f;
+        public static final int signup=0x7f020020;
+        public static final int splash=0x7f020021;
+        public static final int top_panel=0x7f020022;
     }
     public static final class id {
-        public static final int action_settings=0x7f090013;
-        public static final int confirmpass=0x7f09000a;
-        public static final int country=0x7f09000f;
-        public static final int email=0x7f090007;
-        public static final int firstname=0x7f09000e;
-        public static final int imgLogo=0x7f090010;
-        public static final int lastname=0x7f090008;
-        public static final int mobile=0x7f09000b;
-        public static final int password=0x7f090001;
-        public static final int radio0=0x7f09000d;
-        public static final int radio1=0x7f09000c;
-        public static final int radioGroup1=0x7f090004;
-        public static final int signin=0x7f090002;
-        public static final int signout=0x7f090012;
-        public static final int signup=0x7f090003;
-        public static final int signupreg=0x7f090006;
-        public static final int skype=0x7f090009;
-        public static final int spinner=0x7f090005;
-        public static final int username=0x7f090000;
-        public static final int welcome=0x7f090011;
+        public static final int addmedication=0x7f070014;
+        public static final int checkBox1=0x7f070013;
+        public static final int close=0x7f070007;
+        public static final int confirmpass=0x7f07001d;
+        public static final int country=0x7f070018;
+        public static final int email=0x7f07001a;
+        public static final int expandableListView1=0x7f070026;
+        public static final int firstname=0x7f07001c;
+        public static final int header=0x7f070005;
+        public static final int header_back_button=0x7f07000c;
+        public static final int header_home_button_layout=0x7f07000a;
+        public static final int header_layout=0x7f070009;
+        public static final int header_title=0x7f07000b;
+        public static final int imgLogo=0x7f070023;
+        public static final int lastname=0x7f07001b;
+        public static final int lblListHeader=0x7f07000d;
+        public static final int lblListItem=0x7f07000e;
+        public static final int listView1=0x7f070015;
+        public static final int meddir=0x7f070002;
+        public static final int medications=0x7f070027;
+        public static final int medname=0x7f070001;
+        public static final int message=0x7f070008;
+        public static final int mobile=0x7f070019;
+        public static final int parent1=0x7f070004;
+        public static final int password=0x7f070010;
+        public static final int radio0=0x7f070020;
+        public static final int radio1=0x7f070021;
+        public static final int radioGroup1=0x7f07001e;
+        public static final int savebut=0x7f070000;
+        public static final int signin=0x7f07000f;
+        public static final int signout=0x7f070024;
+        public static final int signup=0x7f070012;
+        public static final int signupreg=0x7f070017;
+        public static final int skype=0x7f070022;
+        public static final int spinner=0x7f07001f;
+        public static final int spintext=0x7f070003;
+        public static final int thumbnail=0x7f070016;
+        public static final int title=0x7f070006;
+        public static final int username=0x7f070011;
+        public static final int welcome=0x7f070025;
     }
     public static final class layout {
-        public static final int login=0x7f030000;
-        public static final int register=0x7f030001;
-        public static final int splash=0x7f030002;
-        public static final int welcome=0x7f030003;
-    }
-    public static final class menu {
-        public static final int login=0x7f080000;
+        public static final int addmedication=0x7f030000;
+        public static final int custom_spinner=0x7f030001;
+        public static final int dialog=0x7f030002;
+        public static final int header=0x7f030003;
+        public static final int list_group=0x7f030004;
+        public static final int list_item=0x7f030005;
+        public static final int login=0x7f030006;
+        public static final int medications=0x7f030007;
+        public static final int medslist_row=0x7f030008;
+        public static final int register=0x7f030009;
+        public static final int reminders=0x7f03000a;
+        public static final int splash=0x7f03000b;
+        public static final int welcome=0x7f03000c;
     }
     public static final class string {
-        public static final int action_settings=0x7f050003;
-        public static final int age_picker=0x7f050006;
+        public static final int action_settings=0x7f050004;
+        public static final int age_picker=0x7f050008;
         public static final int app_name=0x7f050000;
-        public static final int email=0x7f050007;
-        public static final int hello_world=0x7f050004;
+        /**     <string-array name="age_array">
+  
+        <item>0-10</item>
+        <item>11-20</item>
+        <item>21-30</item>
+        <item>31-40</item>
+        <item>41-50</item>
+        <item>51-60</item>
+        <item>61-70</item>
+        <item>71-80</item>
+        <item>81-90</item>
+        <item>91-100</item>
+      
+    </string-array>
+         */
+        public static final int email=0x7f050009;
+        public static final int hello_world=0x7f050005;
+        public static final int med_page=0x7f050003;
         public static final int reg_page=0x7f050001;
-        public static final int welcome=0x7f050005;
+        public static final int signout=0x7f050007;
+        public static final int tab_home=0x7f05000a;
+        public static final int tab_medications=0x7f05000b;
+        public static final int welcome=0x7f050006;
         public static final int welcome_page=0x7f050002;
     }
     public static final class style {
@@ -97,10 +169,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
 }
